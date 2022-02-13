9ee8f9d58efb5837acf993072a6cdbff5b2d9366
Roma Bulani <romabulani@gmail.com>
2022-02-14 00:11:07 +0530

    fix : link correction and picture tag removal

--- src/components/wishlist/wishlist.js
+++ src/components/wishlist/wishlist.js
@@ -1,9 +1,9 @@
-const btnCloseArray = document.querySelectorAll('.btn-close-card');
+const btnCloseArray = document.querySelectorAll('.btn-close');
 const arrProducts = Array.from(btnCloseArray).map(btnClose => btnClose.addEventListener('click', onCrossClick));
 
 function onCrossClick(){
     this.parentNode.parentNode.remove();
-    const btnCloseArrayNew = document.querySelectorAll('.btn-close-card');
+    const btnCloseArrayNew = document.querySelectorAll('.btn-close');
     if(btnCloseArrayNew.length === 0)
         document.querySelector('.wishlist-empty-container').style.display = 'flex';
 }
