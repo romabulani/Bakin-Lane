29e1e912290186756f8233e57343249fd6c8d16d
Roma Bulani <romabulani@gmail.com>
2022-02-15 13:33:52 +0530

    feat : added cart

---
+++ src/components/cart/cart.js
@@ -0,0 +1,13 @@
+const btnCloseArray = document.querySelectorAll('.btn-close');
+const arrProducts = Array.from(btnCloseArray).map(btnClose => btnClose.addEventListener('click', onCrossClick));
+
+function onCrossClick(){
+    this.parentNode.remove();
+    const btnCloseArrayNew = document.querySelectorAll('.btn-close');
+    if(btnCloseArrayNew.length === 0){
+        document.querySelector('.cart-empty-container').style.display = 'flex';
+        document.querySelector('.cart-price').style.display = 'none';
+        document.querySelector('.cart-header.heading5').style.display = 'none';
+        document.querySelector('.cart-items').style.borderRight = '0px';
+    }    
+}
