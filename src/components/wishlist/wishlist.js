e587ce650e5be55d245f757c6f90c8d67a2b9241
Roma Bulani <romabulani@gmail.com>
2022-02-16 13:27:21 +0530

    fix : format indentation

--- src/components/wishlist/wishlist.js
+++ src/components/wishlist/wishlist.js
@@ -1,9 +1,11 @@
-const btnCloseArray = document.querySelectorAll('.btn-close');
-const arrProducts = Array.from(btnCloseArray).map(btnClose => btnClose.addEventListener('click', onCrossClick));
+const btnCloseArray = document.querySelectorAll(".btn-close");
+const arrProducts = Array.from(btnCloseArray).map((btnClose) =>
+  btnClose.addEventListener("click", onCrossClick)
+);
 
-function onCrossClick(){
-    this.parentNode.parentNode.remove();
-    const btnCloseArrayNew = document.querySelectorAll('.btn-close');
-    if(btnCloseArrayNew.length === 0)
-        document.querySelector('.wishlist-empty-container').style.display = 'flex';
+function onCrossClick() {
+  this.parentNode.parentNode.remove();
+  const btnCloseArrayNew = document.querySelectorAll(".btn-close");
+  if (btnCloseArrayNew.length === 0)
+    document.querySelector(".wishlist-empty-container").style.display = "flex";
 }
